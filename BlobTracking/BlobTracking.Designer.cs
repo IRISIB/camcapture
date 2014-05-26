04f961a0c425155637ee1e01751103b1a4b9bf18
Steve De Jongh <stdj79@gmail.com>
2014-05-26 23:19:49 +0200

    Added Blob Light tracking

---
+++ BlobTracking/BlobTracking.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace BlobTracking
+{
+    partial class BlobTracking
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.captureImage = new Emgu.CV.UI.ImageBox();
+            this.captureButton = new System.Windows.Forms.Button();
+            this.maskImage = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.captureImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maskImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // captureImage
+            // 
+            this.captureImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.captureImage.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
+            this.captureImage.Location = new System.Drawing.Point(12, 12);
+            this.captureImage.Name = "captureImage";
+            this.captureImage.Size = new System.Drawing.Size(640, 480);
+            this.captureImage.TabIndex = 2;
+            this.captureImage.TabStop = false;
+            // 
+            // captureButton
+            // 
+            this.captureButton.Location = new System.Drawing.Point(12, 498);
+            this.captureButton.Name = "captureButton";
+            this.captureButton.Size = new System.Drawing.Size(100, 30);
+            this.captureButton.TabIndex = 3;
+            this.captureButton.Text = "Start Capture";
+            this.captureButton.UseVisualStyleBackColor = true;
+            this.captureButton.Click += new System.EventHandler(this.captureButton_Click);
+            // 
+            // maskImage
+            // 
+            this.maskImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.maskImage.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
+            this.maskImage.Location = new System.Drawing.Point(658, 12);
+            this.maskImage.Name = "maskImage";
+            this.maskImage.Size = new System.Drawing.Size(640, 480);
+            this.maskImage.TabIndex = 4;
+            this.maskImage.TabStop = false;
+            // 
+            // BlobTracking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1313, 730);
+            this.Controls.Add(this.maskImage);
+            this.Controls.Add(this.captureButton);
+            this.Controls.Add(this.captureImage);
+            this.Name = "BlobTracking";
+            this.Text = "Blob Tracking";
+            ((System.ComponentModel.ISupportInitialize)(this.captureImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maskImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox captureImage;
+        private System.Windows.Forms.Button captureButton;
+        private Emgu.CV.UI.ImageBox maskImage;
+    }
+}
+
---
+++ BlobTracking/BlobTracking.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+// EMGU Needed libraries
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+namespace BlobTracking
+{
+    public partial class BlobTracking : Form
+    {
+
+        private bool _captureRunning = false;               // Used to sets captureButton text
+        private Capture _capture = null;                    // Capture object
+        private Image<Bgr, Byte> _originalImage = null;     // Image directly grabed from camera
+
+        public BlobTracking()
+        {
+            InitializeComponent();
+            try
+            {
+                _capture = new Capture(0);                 // Initialize camera capture
+                _capture.ImageGrabbed += ProcessFrame;      // Image treatement when grabbed
+                
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+        }
+
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            _originalImage = _capture.RetrieveBgrFrame();
+            Image<Gray, Byte> _detectImage = _originalImage.Convert<Gray, Byte>();                  // Converts image to GrayScale
+            _detectImage = _detectImage.ThresholdBinary(new Gray(250), new Gray(255));              // Binary Threshold to keep only high values (near white)
+            _detectImage = _detectImage.Canny(new Gray(255).Intensity, new Gray(255).Intensity);    // Border tracing
+            _detectImage = _detectImage.Dilate(2).Erode(2);                                         // Smooth for cleaner contours detection
+
+
+            MemStorage stor = new MemStorage();
+            Contour<Point> contours = _detectImage.FindContours(CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_SIMPLE, RETR_TYPE.CV_RETR_TREE, stor);  // Detect contours
+
+            Bgr bgrRed = new Bgr(Color.Red);
+            while (contours != null)
+            {
+                // For each contour detected, draw a box and a cross at its center
+                if (contours.BoundingRectangle.Width > 10 && contours.BoundingRectangle.Height > 10)
+                {
+                    Rectangle rect = contours.BoundingRectangle;
+                    PointF baryCenter = new PointF(rect.X + rect.Width / 2, rect.Y + rect.Height / 2);
+                    Cross2DF cross = new Cross2DF(baryCenter,5,5);
+                    _originalImage.Draw(cross, bgrRed, 1);
+                    _originalImage.Draw(rect, bgrRed, 2);
+                }
+
+                contours = contours.HNext;
+            }
+
+
+            maskImage.Image = _detectImage;
+            captureImage.Image = _originalImage;
+        }
+
+
+        private void captureButton_Click(object sender, EventArgs e)
+        {
+            if (_captureRunning)
+            {
+                _capture.Stop();
+                captureButton.Text = "Start Capture";
+            }
+            else
+            {
+                _capture.Start();
+                captureButton.Text = "Stop Capture";
+            }
+            _captureRunning = !_captureRunning;
+        }
+
+
+
+
+
+
+
+
+
+  
+    }
+}
